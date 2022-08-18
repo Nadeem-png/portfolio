eca9268952a8293418e6df1a0864cf3cc65705f7
Nadeem-png <nk27890@gmail.com>
2022-08-18 13:05:42 -0700

    first commit

--- src/component/About.js
+++ src/component/About.js
@@ -3,8 +3,9 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import image from './image.png';
-
-
+import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 export default function About() {
   return (
     <div className='container mt-5 '>
@@ -15,13 +16,21 @@ export default function About() {
       <Typography variant="h4" color="red" component="div">
       Welcome to my Portfolio
         </Typography>
-        <Typography variant="h6" color="" component="div">
+        <Typography variant="h6" color="" component="div" >
         I am Rhinston Jones. A professional photographer. I currently reside at beveryly Hills, North-carolina.
 
 Of course, I love taking  and I have won multiple awards and acquired many certificates
 
 All necessary reference files and documents are available for download
 </Typography>
+<div className='mt-5 '>
+<a href='' ><FacebookRoundedIcon className='ms-5'/></a>
+<a href=''><InstagramIcon className='ms-5'/></a>
+
+<a href=''><YouTubeIcon className='ms-5'/></a>
+</div>
+
+
       </Grid>
       <Grid item xs={6}>
       <img src={image} alt="Logo" className='h-50'/>
