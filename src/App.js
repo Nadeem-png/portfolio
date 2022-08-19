eec4bf6529e65911fceebf916a24ace4b82c783f
Nadeem-png <nk27890@gmail.com>
2022-08-19 12:04:44 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ import Intro from "./components/Intro/Intro";
 import Services from "./components/Services/Services";
 import Experience from "./components/Experience/Experience";
 import Works from "./components/Works/Works";
+import Portfolio from "./components/Portfolio/Portfolio";
 
 function App() {
  
@@ -40,6 +41,7 @@ function App() {
       <Services/>
       <Experience/>
       <Works/>
+      <Portfolio/>
      
     
     </div>
